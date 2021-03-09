07cdf929b3ac9336a7b219d4bd0469878fe9008f
Peter Xia <aattyy@gmail.com>
2021-03-08 20:26:52 -0800

    remove diffs

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/env bash
 
+
+prev=$(ls snapshots | sort -r | head -n1 | xargs)
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     
+		today=$(TZ='America/Los_Angeles' date -Idate);;
+    Darwin*)
+		realpath() {
+		  OURPWD=$PWD
+		  cd "$(dirname "$1")"
+		  LINK=$(readlink "$(basename "$1")")
+		  while [ "$LINK" ]; do
+		    cd "$(dirname "$LINK")"
+		    LINK=$(readlink "$(basename "$1")")
+		  done
+		  REALPATH="$PWD/$(basename "$1")"
+		  cd "$OURPWD"
+		  echo "$REALPATH"
+		}
+		# realpath "$@"
+		today=$(TZ='America/Los_Angeles' date +%F);;
+    CYGWIN*)    exit 1;;
+    MINGW*)     exit 1;;
+    *)          exit 1;;
+esac
+
 root=$(realpath $(dirname "$0")/..)
 cd $root
 
-prev=$(ls snapshots | sort -r | head -n1 | xargs)
-today=$(TZ='America/Los_Angeles' date -Idate)
+echo ${today}
+
+# today=$(TZ='America/Los_Angeles' date -Idate)
 
 download_dir="$root"/snapshots/"$today"
 mkdir -p "$download_dir"
@@ -19,16 +47,3 @@ curl -L 'https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INN
 
 curl -L 'https://ark-funds.com/wp-content/fundsiteliterature/csv/THE_3D_PRINTING_ETF_PRNT_HOLDINGS.csv' -o prnt.csv
 curl -L 'https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_ISRAEL_INNOVATIVE_TECHNOLOGY_ETF_IZRL_HOLDINGS.csv' -o izrl.csv
-
-cd "$root"
-
-changes_dir="$root"/changes/"$today"
-mkdir -p "$changes_dir"
-
-for fund in ark{k,q,w,g,l}; do
- scripts/csv_diff.py -l snapshots/"$prev"/$fund.csv -r snapshots/"$today"/$fund.csv -o "$changes_dir"/$fund.csv
-done
-
-scripts/csv_diff.py -l snapshots/"$prev"/ark{k,q,w,g,f}.csv -r snapshots/"$today"/ark{k,q,w,g,f}.csv -o "$changes_dir"/combined_long.csv
-scripts/csv_diff.py -r snapshots/"$prev"/ark{k,q,w,g,f}.csv -l snapshots/"$today"/ark{k,q,w,g,f}.csv -o "$changes_dir"/combined_short.csv
-
